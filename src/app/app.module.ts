0075c207b4e1c13afb92ed4cb8bff199c0e5bf49
Garik Arakelyan <garik.arakelyan92@gmail.com>
2020-08-08 16:18:00 +0400

    added routing and create cart,layout pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    MainPageComponent
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
